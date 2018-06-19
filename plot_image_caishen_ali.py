fbab06af8147f6ca219aa906fe7ed85d643c59a2
yanshuo <yanshuo5091@163.com>
2018-06-19 12:29:51 +0800

    20180312

--- plot_image_caishen_ali.py
+++ plot_image_caishen_ali.py
@@ -5,7 +5,7 @@ import os
 import time
 import re
 import tkFileDialog
-import numpy as np
+import numpy
 import Tkinter
 import matplotlib.pyplot as plyt
 
@@ -25,21 +25,35 @@ def get_result_dir():
 def plyt_1jobs(item_filename):
     data_1 = []
     data_2 = []
-    filename_list_handled.append(item_filename)
+    #filename_list_handled.append(item_filename)
     datapathname = os.path.join(data_dir, item_filename)
     figure_name = item_filename.split('.')[0]
     file1 = open(datapathname, mode='r')
     for item_data in file1:
-        temp = item_data.split(',')[1]
+        temp = item_data.split(',')[1].strip()
+        #temp_time = item_data.split(',')[0].strip()
         data_1.append(temp)
+        #data_2.append(temp_time)
     length = len(data_1)
-    for i in range(1, length + 1):
+    #j = 0
+    for i in range(1, length +1):
         data_2.append(i)
-    data_second = np.array(data_2)
-    data_data = np.array(data_1)
+        # j = j + 1
+    print len(data_2)
+    # find max & min for y stick,set jiange 1/10
+    max_y = float(max(data_1))
+    min_y = float(min(data_1))
+    print "max:%s;min:%s" % (max_y, min_y)
+    jiange = (max_y - min_y) / 10
+    y_ticks = numpy.arange(min_y, max_y, step=jiange)
+    print y_ticks
+    data_second = numpy.array(data_2)
+    data_data = numpy.array(data_1)
     file1.close()
-    figure_1 = plyt.figure(item_filename)
-    plyt.title(item_filename)
+    figure_1 = plyt.figure(figure_name)
+    plyt.title(figure_name)
+    plyt.ylim((min_y, max_y))
+
     plyt.plot(data_second, data_data)
     time.sleep(1)
     figure_1.savefig(os.path.join(result_dir, figure_name))
@@ -73,8 +87,8 @@ def plyt_4jobs(item_filename, sub_filename_list_all):
     length = len(data_1)
     for i in range(1, length + 1):
         data_2.append(i)
-    data_second = np.array(data_2)
-    data_data = np.array(data_1)
+    data_second = numpy.array(data_2)
+    data_data = numpy.array(data_1)
     figure_1 = plyt.figure(figure_name)
     plyt.title(figure_name)
     plyt.plot(data_second, data_data)
@@ -120,8 +134,8 @@ def plyt_32jobs(item_filename, sub_filename_list_all):
     length = len(data_1)
     for i in range(1, length + 1):
         data_2.append(i)
-    data_second = np.array(data_2)
-    data_data = np.array(data_1)
+    data_second = numpy.array(data_2)
+    data_data = numpy.array(data_1)
     figure_1 = plyt.figure(figure_name)
     plyt.title(figure_name)
     plyt.plot(data_second, data_data)
@@ -136,24 +150,24 @@ def plot_image():
     pattern_find_4job = re.compile(r'_4_')
     pattern_find_32job = re.compile(r'_32_')
     for item_filename in filename_list:
+        print item_filename
         if item_filename.split(".")[0] not in filename_done:
             if_4jobs = re.search(pattern_find_4job, item_filename)
-            if if_4jobs is None:
-                if_32jobs = re.search(pattern_find_32job, item_filename)
-                if if_32jobs is None:
-                    print "fail.not find%s" % item_filename
-                    pass
-                else:
-                    plyt_32jobs(item_filename, filename_list)
-                    filename_done.append(item_filename.split(".")[0])
-
-            else:
+            if if_4jobs is not None:
                 if item_filename not in filename_list_handled:
                     plyt_4jobs(item_filename, filename_list)
                     filename_done.append(item_filename.split(".")[0])
-
-
-
+            else:
+                if_32jobs = re.search(pattern_find_32job, item_filename)
+                if if_32jobs is not None:
+                    #print "fail.not find%s" % item_filename
+                    #plot 1job
+                    plyt_32jobs(item_filename, filename_list)
+                    filename_done.append(item_filename.split(".")[0])
+                else:
+                    print "Begin %s" % item_filename
+                    plyt_1jobs(item_filename)
+                    filename_done.append(item_filename)
 
 root = Tkinter.Tk()
 root.title("阿里性能测试单盘结果画图工具".decode('gbk'))
@@ -173,7 +187,7 @@ frame_top_bottom = Tkinter.Frame(frame_top, height=20)
 frame_top_bottom.pack()
 Tkinter.Label(frame_top_top, text='请在如下选择需要处理的包含阿里性能测试结果的目录'.decode('gbk'), bg='Red').pack()
 Tkinter.Entry(frame_top_bottom, textvariable=var_char_entry_data_dir, width=40).pack(side=Tkinter.LEFT)
-Tkinter.Button(frame_top_bottom, text='选择文件'.decode('gbk'), command=get_data_dir, width=20).pack(side=Tkinter.RIGHT)
+Tkinter.Button(frame_top_bottom, text='选择文件夹'.decode('gbk'), command=get_data_dir, width=20).pack(side=Tkinter.RIGHT)
 
 frame_middle = Tkinter.Frame(root, height=20)
 frame_middle.pack()
@@ -183,7 +197,7 @@ frame_middle_bottom = Tkinter.Frame(frame_middle, height=20)
 frame_middle_bottom.pack()
 Tkinter.Label(frame_middle_top, text='请在如下选择放置最后生成图片的目录'.decode('gbk'), bg='Red').pack()
 Tkinter.Entry(frame_middle_bottom, textvariable=var_char_entry_result_dir, width=40).pack(side=Tkinter.LEFT)
-Tkinter.Button(frame_middle_bottom, text='选择文件'.decode('gbk'), command=get_result_dir, width=20).pack(side=Tkinter.RIGHT)
+Tkinter.Button(frame_middle_bottom, text='选择文件夹'.decode('gbk'), command=get_result_dir, width=20).pack(side=Tkinter.RIGHT)
 
 frame_button = Tkinter.Frame(root, height=20)
 frame_button.pack()
